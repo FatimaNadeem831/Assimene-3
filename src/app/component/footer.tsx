b02ebdc97b1932d09a28064d0d465b87f5591d28
FatimaNadeem831 <167708955+FatimaNadeem831@users.noreply.github.com>
2024-10-31 22:42:15 +0500

    Add files via upload

---
+++ src/app/component/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-purple-200">
+      <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-8 mx-auto  flex items-center sm:flex-row flex-col">
+      <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 ">
+        <span className="ml-3 text-xl text-purple-700">FATIMA NADEEM</span>
+    </a>
+  </div>
+</footer>
+    </div>
+  );
+};
+
+export default Footer
